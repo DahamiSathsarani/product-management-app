682539753dab44759c3fcbbaa99fddff1f272093
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2026-01-10 15:28:07 +0530

    Add product table & crud apis

---
+++ backend/product_api/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table      = 'products'; 
+    protected $primaryKey = 'id';
+    public $incrementing  = true; 
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'price',
+        'is_active',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class, 'category_id');
+    }
+}
