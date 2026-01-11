cc04ac8c6beecfa09825e10133f94d882feaeb0e
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2026-01-11 17:19:02 +0530

    Update product screen grouping by categories

--- frontend/product_mobile_app/lib/app/app_colors.dart
+++ frontend/product_mobile_app/lib/app/app_colors.dart
@@ -4,5 +4,5 @@ class AppColors {
   static const Color blackColor = Color(0xFF01010D);
   static const Color primaryColor = Color(0xFF01D462);
   static const Color secondaryColor = Color(0xFF96FFC6);
-  static const Color greyColor = Color.fromARGB(255, 223, 223, 223);
+  static const Color greyColor = Color.fromARGB(255, 233, 233, 233);
 }
